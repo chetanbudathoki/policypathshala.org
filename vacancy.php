8a94baf67575e5308e7421a207eefff118989900
Chetan Budathoki <chetanbudathoki@outlook.com>
2024-09-06 11:57:19 +0545

    "backup"

---
+++ vacancy.php
@@ -0,0 +1,182 @@
+<!doctype html>
+<html lang="zxx">
+<?php include 'head.php';?>	
+
+    <body>
+		<!-- Start Preloader Area -->
+		<div class="preloader">
+			<div class="lds-ripple">
+				<div class="pl-flip-1 pl-flip-2"></div>
+			</div>
+		</div>
+		<!-- End Preloader Area -->
+		
+		<!-- Start Header Area -->
+		<?php include 'header.php';?>
+		<!-- End Header Area -->
+
+		<!-- Search Modal -->
+		<div class="modal fade search-modal-area" id="exampleModalsrc">
+			<div class="modal-dialog modal-dialog-centered">
+				<div class="modal-content">
+					<button type="button" data-bs-dismiss="modal" class="closer-btn">
+						<i class="ri-close-line"></i>
+					</button>
+
+					<div class="modal-body">
+						<form class="search-box">
+							<div class="search-input">
+								<input type="text" name="Search" placeholder="Search for..." class="form-control">
+
+								<button type="submit" class="search-btn">
+									<i class="ri-search-line"></i>
+								</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- Search Modal -->
+
+		<!-- Start Page Title Area -->
+		<div class="page-title-area bg-9">
+			<div class="container">
+				<div class="page-title-content">
+					<h2>Vacancy Announcement- Damak Municipality</h2>
+
+					<ul>
+						<li>
+							<a href="index.php">
+								Home 
+							</a>
+						</li>
+
+						<li class="active">Vacancy Announcement</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<!-- End Page Title Area -->
+
+		<!-- Start Campus Experience Area -->
+		
+		<!-- End Campus Experience Area -->
+
+		<!-- Start Admissions Area -->
+		<section class="admissions-area ptb-100">
+			<div class="container">
+				<div class="main-default-content">
+					<p>Publication Date: 17 March 2024 </p>
+					
+					<h3>Position: Public Health Officer </h3>
+					<p>Background: Public Policy Pathshala (PPP) has partnered with Australian Government, Department of Foreign Affairs and Trade (DFAT), The Asia Foundation – Sub-national Governance Program (SNGP) to promote and strengthen public health system governance at the local level with institutionalization of necessary health legislations and capacitating them on their health sector programming and functions to effectively implement the mandates toward improved access to quality public health services. The project entitled “Strengthening local public & environmental health governance system” aims to enhance the support in formulating and implementing health legislations and policies at the local level by orienting, capacitating, mentoring, and fostering increased accountability and ownership. In this year, the program's effort is directed towards bolstering public health system governance integrating environment health by fully committing to implementing the project, prioritizing inclusivity, and adopting an environmentally sensitive approach.</p>
+				
+					<div class="gap-20"></div>
+
+					<h3>Roles and Responsibilities: </h3>
+					<ul>
+						<li>Collaborate with the PPP program team as needed to support project implementation and foster favorable relations with the municipality. </li>
+						<li>Provide mentoring support to facilitate regular meetings of the Social Development Committee (SDC), Health Facility Operation & Management Committee, environment committees and other public health working committees. </li>
+						<li>Collaborate with municipal stakeholders, including environmental committees and health officials, to gather inputs and align policies with local health needs and priorities. </li>
+						<li>Coordinate the implementation of public health policies, ensuring adherence to guidelines and protocols at various levels.  </li>
+						<li>Monitor implementation progress, identify challenges, and propose contextual solutions to overcome them. </li>
+						<li>Support training, mentorship, and development of resources for the local health workforce on policy-related topics. </li>
+						<li>Coordinate data collection from health facilities to support on policy planning, development, and monitoring purposes.  </li>
+						<li>Analyze collected data to assess policy impact and effectiveness, identifying areas for improvement. </li>
+						<li>Prepare reports and presentations summarizing key findings and recommendations from data analysis.  </li>
+						<li>Maintain close coordination with the municipality's health section for effective collaboration and communication. </li>
+						<li>Provide regular monthly reports on policy formulation and implementation, including challenges, successes, and recommendations for improvement.  </li>
+
+					
+					</ul>
+					<div class="gap-20"></div>
+
+					<h3>Qualifications and Requirements: </h3>
+					<ul>
+						<li>Bachelor's degree in public health with at least two years working experience in the relevant area. </li>
+						<li>Good understanding on health governance, policy-making, and implementation.</li>
+						<li>Excellent facilitation, communication, interpersonal and report writing skills. </li>
+						<li>Time management skills and excellent organizational skills. </li>
+						<li>Demonstrated ability to work collaboratively. </li>
+						<li>Local applicants will be given higher priority.</li>
+						
+					</ul>
+					<div class="gap-20"></div>
+
+					<h3>Duration and Location:</h3>
+					<p>The position is for a 9-month contract with possibility of extension. The Public Health Officer will be stationed primarily at the Damak municipality, with occasional travel for project activities. 
+					
+					<div class="gap-20"></div>
+
+					<h3>Application Process:</h3>
+					<p>Interested candidates are invited to apply by submitting the following documents within 7 days of this announcement: </p>
+					<ul>
+						<li>Updated CV with relevant credentials. </li>
+						<li>Recent photograph. </li>
+						<li>Motivational letter highlighting interest, qualifications, and experience. </li>
+						<li>Public health licensure. </li>							
+					</ul>
+					<div class="gap-20"></div>
+
+					<h3>Submission Method:</h3>
+					<p>Applications should be sent to Public Policy Pathshala through their email “info@policypathshala.org”. Please ensure that the subject line indicates "[Job Position] and in the Application - [Your Name]".</p>
+					<div class="gap-20"></div>
+
+					<h4>Note:</h4>
+					<ul>
+						<li>Our project values diversity and strives for gender equality, diversity, and social inclusion (GEDSI) in its workforce.  </li>
+						<li>Only shortlisted candidates will be contacted for further processes. </li>
+						<li>Inquiries regarding the application status will not be entertained.  </li>
+												
+					</ul>
+					
+					<br>
+					<p>Thank you for your interest in joining our team. </p>
+
+				</div>
+			</div>
+		</section>
+		<!-- End Admissions Area -->
+		<?php include 'footer.php';?>
+		<!-- Start Footer Area -->
+		
+		<!-- End Copy Right Area -->
+		
+		<!-- Start Go Top Area -->
+		<div class="go-top">
+			<i class="ri-arrow-up-s-fill"></i>
+			<i class="ri-arrow-up-s-fill"></i>
+		</div>
+		<!-- End Go Top Area -->
+
+        <!-- Jquery Min JS -->
+        <script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="assets/js/jquery.min.js"></script> 
+        <!-- Bootstrap Bundle Min JS -->
+        <script src="assets/js/bootstrap.bundle.min.js"></script>
+        <!-- Meanmenu Min JS -->
+		<script src="assets/js/meanmenu.min.js"></script>
+		<!-- Owl Carousel Min JS -->
+		<script src="assets/js/owl.carousel.min.js"></script>
+		<!-- Wow Min JS -->
+        <script src="assets/js/wow.min.js"></script>
+		<!-- Appear Min JS -->
+        <script src="assets/js/appear.min.js"></script>
+		<!-- Odometer Min JS -->
+        <script src="assets/js/odometer.min.js"></script>
+		<!-- Jarallax Min JS -->
+        <script src="assets/js/jarallax.min.js"></script>
+		<!-- Bootstrap Datepicker Min JS -->
+        <script src="assets/js/bootstrap-datepicker.min.js"></script>
+		<!-- Magnific Popup Min JS -->
+        <script src="assets/js/magnific-popup.min.js"></script>
+		<!-- Form Validator Min JS -->
+		<script src="assets/js/form-validator.min.js"></script>
+		<!-- Contact JS -->
+		<script src="assets/js/contact-form-script.js"></script>
+		<!-- Ajaxchimp Min JS -->
+		<script src="assets/js/ajaxchimp.min.js"></script>
+        <!-- Custom JS -->
+		<script src="assets/js/custom.js"></script>
+    </body>
+</html>
